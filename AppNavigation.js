751daea9f58ab7e4407747c5104b0c9e47bda859
Diego Guerrero <dgr13gcu@gmail.com>
2025-12-09 01:26:18 -0600

    Create AppNavigation.js

---
+++ AppNavigation.js
@@ -0,0 +1,19 @@
+// src/navigation/AppNavigator.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import DetailScreen from '../screens/DetailScreen';
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Lista de Productos' }} />
+        <Stack.Screen name="Detail" component={DetailScreen} options={{ title: 'Detalle' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
